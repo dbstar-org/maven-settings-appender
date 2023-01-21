4da736a064b52a09da93bdbe8a84e8036fdf7789
代波 <dbstar@y1cloud.com>
2023-01-22 00:49:25 +0800

    基础功能

--- appender.sh
+++ appender.sh
@@ -3,6 +3,8 @@ ITEM=$2
 CONTENT=$3
 
 echo "add to $ITEM use: $CONTENT"
+echo grep -n "^<\/settings>$" "$SETTINGS_FILE"
+grep -n "^<\/settings>$" "$SETTINGS_FILE"
 echo grep -n "^  <\/$ITEM>$" "$SETTINGS_FILE"
 grep -n "^  <\/$ITEM>$" "$SETTINGS_FILE"
 echo 'ok!'
