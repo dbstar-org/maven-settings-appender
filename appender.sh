6c77e503ace8f7da958306da6936e290ad310f82
代波 <dbstar@y1cloud.com>
2023-01-22 17:17:31 +0800

    测试appender.sh

--- appender.sh
+++ appender.sh
@@ -6,10 +6,21 @@ CONTENT=$3
 
 findLineNumber() {
   echo grep -n "$2" "$1"
-  grep -n "$2" "$1"
+  lines=$(grep -c "$2" "$1")
+  if [ $lines -eq 0 ]; then
+    return 0
+  elif [ $lines -ne 1 ]; then
+    return -1
+  else
+    expr $(grep -n "$2" "$1" | awk -F ':' '{print $1}')
+  fi
 }
 
 echo "add to $ITEM use: $CONTENT"
 findLineNumber "$SETTINGS_FILE" "^<\/settings>$"
+echo $?
 findLineNumber "$SETTINGS_FILE" "^  <\/$ITEM>$"
+echo $?
+findLineNumber "$SETTINGS_FILE" "settings>"
+echo $?
 echo 'ok!'
