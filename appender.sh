f4fdf611b7ce80a9a09aea9b7c370db5ff70e990
代波 <dbstar@y1cloud.com>
2023-01-22 18:43:42 +0800

    测试appender.sh

--- appender.sh
+++ appender.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SETTINGS_FILE=$1
+SETTINGS_PATH=$(dirname "$SETTINGS_FILE")
 ITEM=$2
 CONTENT=$3
 
@@ -19,13 +20,29 @@ findLineNumber() {
   esac
 }
 
+NEW_SETTINGS_FILE=${SETTINGS_FILE}-$(echo $RANDOM)
+echo "new settings.xml is $NEW_SETTINGS_FILE"
+
 lines=$(findLineNumber "$SETTINGS_FILE" "^  <\/$ITEM>$") || exit 1
-echo "$?: $lines"
+if [ '' == '$lines' ]; then
+  echo "[<$ITEM>] not exist in $SETTINGS_FILE, created."
 
-lines=$(findLineNumber "$SETTINGS_FILE" "^<\/settings>$") || exit 1
-echo "$?: $lines"
+  lines=$(findLineNumber "$SETTINGS_FILE" "^<\/settings>$") || exit 1
+  if [ '' == '$lines' ]; then
+    echo "[<settings>] not found in $SETTINGS_FILE, exited."
+    exit 2
+  fi
+  echo "find [<settings>] at [$lines] lines in $SETTINGS_FILE."
+  head -n $(expr "$lines" - 1) $SETTINGS_FILE >$NEW_SETTINGS_FILE
+  echo "  <$ITEM>" >>$NEW_SETTINGS_FILE
+  echo "$CONTENT" >>$NEW_SETTINGS_FILE
+  echo "  <\/$ITEM>" >>$NEW_SETTINGS_FILE
+  tail -n $(expr `wc -l $SETTINGS_FILE | awk '{print $1}'` - "$lines") $SETTINGS_FILE >>$NEW_SETTINGS_FILE
+else
+  echo "find [<$ITEM>] at [$lines] lines in $SETTINGS_FILE."
+  head -n $(expr "$lines" - 1) $SETTINGS_FILE >$NEW_SETTINGS_FILE
+  echo "$CONTENT" >>$NEW_SETTINGS_FILE
+  tail -n $(expr `wc -l $SETTINGS_FILE | awk '{print $1}'` - "$lines") $SETTINGS_FILE >>$NEW_SETTINGS_FILE
+fi
 
-lines=$(findLineNumber "$SETTINGS_FILE" "settings") || exit 1
-echo "$?: $lines"
-
-echo 'ok!'
+cat "$NEW_SETTINGS_FILE"
