7c3b68274afd243100fae7377e4ed5ce95f620f6
代波 <dbstar@y1cloud.com>
2023-01-22 23:02:28 +0800

    增加日志输出

--- appender.sh
+++ appender.sh
@@ -20,28 +20,39 @@ findLineNumber() {
 }
 
 NEW_SETTINGS_FILE=${SETTINGS_FILE}-$(echo $RANDOM)
-echo "create new temp settings.xml: $NEW_SETTINGS_FILE"
+echo "create new temp settings.xml file: $NEW_SETTINGS_FILE"
 
 lines=$(findLineNumber "$SETTINGS_FILE" "^  <\/$ITEM>$") || exit 1
 if [ -z "$lines" ]; then
-  echo "[<$ITEM>] not exist in $SETTINGS_FILE, created."
+  echo "[</$ITEM>] not exist in $SETTINGS_FILE, created."
 
   lines=$(findLineNumber "$SETTINGS_FILE" "^<\/settings>$") || exit 1
   if [ -z "$lines" ]; then
-    echo "[<settings>] not found in $SETTINGS_FILE, exited."
+    echo "[</settings>] not found in $SETTINGS_FILE, exited with error."
     exit 2
   fi
-  echo "find [<settings>] at [$lines] lines in $SETTINGS_FILE."
+  echo "find [</settings>] at [$lines] line in $SETTINGS_FILE."
+  echo "copy top $(expr "$lines" - 1) lines of $SETTINGS_FILE to $NEW_SETTINGS_FILE."
   head -n $(expr "$lines" - 1) $SETTINGS_FILE >$NEW_SETTINGS_FILE
+  echo "append <$ITEM> to $NEW_SETTINGS_FILE."
   echo "  <$ITEM>" >>$NEW_SETTINGS_FILE
+  echo "append content to $NEW_SETTINGS_FILE."
   echo "$CONTENT" | awk '{print "    "$0}' >>$NEW_SETTINGS_FILE
+  echo "append </$ITEM> to $NEW_SETTINGS_FILE."
   echo "  </$ITEM>" >>$NEW_SETTINGS_FILE
+  echo "copy remaining lines from $lines line of $SETTINGS_FILE to $NEW_SETTINGS_FILE."
   tail -n +$lines $SETTINGS_FILE >>$NEW_SETTINGS_FILE
 else
-  echo "find [<$ITEM>] at [$lines] line in $SETTINGS_FILE."
+  echo "find [</$ITEM>] at [$lines] line in $SETTINGS_FILE."
+  echo "copy top $(expr "$lines" - 1) lines of $SETTINGS_FILE to $NEW_SETTINGS_FILE."
   head -n $(expr "$lines" - 1) $SETTINGS_FILE >$NEW_SETTINGS_FILE
+  echo "append content to $NEW_SETTINGS_FILE."
   echo "$CONTENT" | awk '{print "    "$0}' >>$NEW_SETTINGS_FILE
+  echo "copy remaining lines from $lines line of $SETTINGS_FILE to $NEW_SETTINGS_FILE."
   tail -n +$lines $SETTINGS_FILE >>$NEW_SETTINGS_FILE
 fi
 
-cat -n "$NEW_SETTINGS_FILE"
+echo "overwrite $SETTINGS_FILE with $NEW_SETTINGS_FILE"
+cp -f "$NEW_SETTINGS_FILE" "$SETTINGS_FILE"
+echo "delete temp settings.xml file: $NEW_SETTINGS_FILE"
+rm -f "$NEW_SETTINGS_FILE"
